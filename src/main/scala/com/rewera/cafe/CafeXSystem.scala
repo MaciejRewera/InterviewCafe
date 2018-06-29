21732960b5ab6e975a34ca6b1319d469cdde245a
Maciek <ReweraM@gmail.com>
2018-06-29 11:14:39 +0200

    Added properties for orders and adjusted Unit Tests

--- src/main/scala/com/rewera/cafe/CafeXSystem.scala
+++ src/main/scala/com/rewera/cafe/CafeXSystem.scala
@@ -1,19 +1,39 @@
 package com.rewera.cafe
 
+sealed trait Item
+case object Cola extends Item
+case object Coffee extends Item
+case object CheeseSandwich extends Item
+case object SteakSandwich extends Item
+
+sealed trait ItemType
+case object Drink extends ItemType
+case object Food extends ItemType
+
+sealed trait ServingTemperature
+case object Cold extends ServingTemperature
+case object Hot extends ServingTemperature
+
+case class ItemProperties(cost: BigDecimal, itemType: ItemType, servingTemperature: ServingTemperature)
+
 
 object CafeXSystem {
 
-  val itemsPrices: Map[String, BigDecimal] = Map(
-    "Cola" -> BigDecimal(0.5),
-    "Coffee" -> BigDecimal(1.0),
-    "Cheese Sandwich" -> BigDecimal(2.0),
-    "Steak Sandwich" -> BigDecimal(4.5),
+  val itemsProperties: Map[Item, ItemProperties] = Map(
+    Cola -> ItemProperties(BigDecimal(0.5), Drink, Cold),
+    Coffee -> ItemProperties(BigDecimal(1.0), Drink, Hot),
+    CheeseSandwich -> ItemProperties(BigDecimal(2.0), Food, Cold),
+    SteakSandwich -> ItemProperties(BigDecimal(4.5), Food, Hot),
   )
 
-  def calcTotalBill(purchasedItems: Seq[String]): BigDecimal =
-    purchasedItems.map(itemsPrices(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  def calcTotalBill(purchasedItems: Seq[Item]): BigDecimal = {
+    val noChargeCost =
+      purchasedItems.map(itemsProperties(_).cost).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP)
 
-  def calcServiceCharge(purchasedItems: Seq[String]): BigDecimal = {
+    noChargeCost + calcServiceCharge(purchasedItems)
+  }
+
+  def calcServiceCharge(purchasedItems: Seq[Item]): BigDecimal = {
     0
   }
 
--- src/test/scala/com/rewera/cafe/CafeXSystemTest.scala
+++ src/test/scala/com/rewera/cafe/CafeXSystemTest.scala
@@ -4,18 +4,22 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class CafeXSystemTest extends FlatSpec with Matchers {
 
-  "A CafeXSystem" should "return proper cost per item" in {
-    val item_1 = List("Cola")
-    CafeXSystem.calcTotalBill(item_1) shouldEqual BigDecimal(0.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  "A CafeXSystem" should "return proper properties per item" in {
+    CafeXSystem.itemsProperties(Cola).cost shouldEqual BigDecimal(0.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    CafeXSystem.itemsProperties(Cola).itemType shouldEqual Drink
+    CafeXSystem.itemsProperties(Cola).servingTemperature shouldEqual Cold
 
-    val item_2 = List("Coffee")
-    CafeXSystem.calcTotalBill(item_2) shouldEqual BigDecimal(1.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    CafeXSystem.itemsProperties(Coffee).cost shouldEqual BigDecimal(1.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    CafeXSystem.itemsProperties(Coffee).itemType shouldEqual Drink
+    CafeXSystem.itemsProperties(Coffee).servingTemperature shouldEqual Hot
 
-    val item_3 = List("Cheese Sandwich")
-    CafeXSystem.calcTotalBill(item_3) shouldEqual BigDecimal(2.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    CafeXSystem.itemsProperties(CheeseSandwich).cost shouldEqual BigDecimal(2.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    CafeXSystem.itemsProperties(CheeseSandwich).itemType shouldEqual Food
+    CafeXSystem.itemsProperties(CheeseSandwich).servingTemperature shouldEqual Cold
 
-    val item_4 = List("Steak Sandwich")
-    CafeXSystem.calcTotalBill(item_4) shouldEqual BigDecimal(4.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    CafeXSystem.itemsProperties(SteakSandwich).cost shouldEqual BigDecimal(4.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    CafeXSystem.itemsProperties(SteakSandwich).itemType shouldEqual Food
+    CafeXSystem.itemsProperties(SteakSandwich).servingTemperature shouldEqual Hot
   }
 
   it should "return 0 for empty purchases list" in {
@@ -25,15 +29,15 @@ class CafeXSystemTest extends FlatSpec with Matchers {
       BigDecimal(0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
   }
 
-  it should "return 1.5 for items [Cola, Coffee,]" in {
-    val itemsPurchased = List("Cola", "Coffee")
+  it should "return 1.5 for items [Cola, Coffee]" in {
+    val itemsPurchased = List(Cola, Coffee)
 
     CafeXSystem.calcTotalBill(itemsPurchased) shouldEqual
       BigDecimal(1.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
   }
 
   it should "charge 10% for service when cold food was ordered" in {
-    val itemsPurchased = List("Cola", "Cheese Sandwich")
+    val itemsPurchased = List(Cola, CheeseSandwich)
 
     CafeXSystem.calcServiceCharge(itemsPurchased) shouldEqual
       BigDecimal(0.25).setScale(2, BigDecimal.RoundingMode.HALF_UP)
@@ -43,7 +47,7 @@ class CafeXSystemTest extends FlatSpec with Matchers {
   }
 
   it should "charge 20% for service when hot food was ordered" in {
-    val itemsPurchased = List("Cola", "Steak Sandwich")
+    val itemsPurchased = List(Cola, SteakSandwich)
 
     CafeXSystem.calcServiceCharge(itemsPurchased) shouldEqual
       BigDecimal(1.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
@@ -53,7 +57,7 @@ class CafeXSystemTest extends FlatSpec with Matchers {
   }
 
   it should "charge 20% for service when both cold and hot food was ordered" in {
-    val itemsPurchased = List("Cola", "Steak Sandwich", "Cheese Sandwich")
+    val itemsPurchased = List(Cola, SteakSandwich, CheeseSandwich)
 
     CafeXSystem.calcServiceCharge(itemsPurchased) shouldEqual
       BigDecimal(1.4).setScale(2, BigDecimal.RoundingMode.HALF_UP)
@@ -63,8 +67,8 @@ class CafeXSystemTest extends FlatSpec with Matchers {
   }
 
   it should "not exceed 20.0 for service when hot food was ordered" in {
-    val coffees = List.fill(200)("Coffee")
-    val itemsPurchased = "Steak Sandwich" :: coffees
+    val coffees = List.fill(200)(Coffee)
+    val itemsPurchased = SteakSandwich :: coffees
 
     CafeXSystem.calcServiceCharge(itemsPurchased) shouldEqual
       BigDecimal(20.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
