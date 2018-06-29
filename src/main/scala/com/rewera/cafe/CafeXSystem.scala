e456aa748829eea3dcad2e8f3db899f7b27f3e5f
Maciek <ReweraM@gmail.com>
2018-06-29 10:28:28 +0200

    Added Unit Tests with bill calculation

--- src/main/scala/com/rewera/cafe/CafeXSystem.scala
+++ src/main/scala/com/rewera/cafe/CafeXSystem.scala
@@ -1,9 +1,16 @@
 package com.rewera.cafe
 
 
-
 object CafeXSystem {
 
-  def calcTotalBill(purchasedItems: Seq[String]): BigDecimal = null
+  val itemsPrices: Map[String, BigDecimal] = Map(
+    "Cola" -> BigDecimal(0.5),
+    "Coffee" -> BigDecimal(1.0),
+    "Cheese Sandwich" -> BigDecimal(2.0),
+    "Steak Sandwich" -> BigDecimal(4.5),
+  )
+
+  def calcTotalBill(purchasedItems: Seq[String]): BigDecimal =
+    purchasedItems.map(itemsPrices(_)).sum.setScale(2, BigDecimal.RoundingMode.HALF_UP)
 
 }
--- src/test/scala/com/rewera/cafe/CafeXSystemTest.scala
+++ src/test/scala/com/rewera/cafe/CafeXSystemTest.scala
@@ -4,4 +4,35 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class CafeXSystemTest extends FlatSpec with Matchers {
 
+  "A CafeXSystem" should "return proper cost per item" in {
+    val item_1 = List("Cola")
+    CafeXSystem.calcTotalBill(item_1) shouldEqual BigDecimal(0.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+
+    val item_2 = List("Coffee")
+    CafeXSystem.calcTotalBill(item_2) shouldEqual BigDecimal(1.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+
+    val item_3 = List("Cheese Sandwich")
+    CafeXSystem.calcTotalBill(item_3) shouldEqual BigDecimal(2.0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+
+    val item_4 = List("Steak Sandwich")
+    CafeXSystem.calcTotalBill(item_4) shouldEqual BigDecimal(4.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  }
+
+  it should "return 0 for empty purchases list" in {
+    val itemsPurchased = List()
+
+    CafeXSystem.calcTotalBill(itemsPurchased) shouldEqual
+      BigDecimal(0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  }
+
+  it should "return 3.5 for items [\"Cola\", \"Coffee\", \"Cheese Sandwich\"]" in {
+    val itemsPurchased = List("Cola", "Coffee", "Cheese Sandwich")
+
+    CafeXSystem.calcTotalBill(itemsPurchased) shouldEqual
+      BigDecimal(3.5).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  }
+
+
+
+
 }
